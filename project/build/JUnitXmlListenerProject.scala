a498d96cfbed8abbc6c69237b6e2941b8c6005cc
Christoph Henkelmann <christoph@henkelmann.eu>
2010-12-12 13:34:38 +0100

    updated build.properties

--- project/build/JUnitXmlListenerProject.scala
+++ project/build/JUnitXmlListenerProject.scala
@@ -3,5 +3,5 @@ import sbt._
 
 class JUnitXmlListenerProject(info: ProjectInfo) extends PluginProject(info) {
     override def managedStyle = ManagedStyle.Maven
-    lazy val publishTo = Resolver.ssh("Christoph's Maven Repo", "taupo", "/srv/maven/")
+    lazy val publishTo = Resolver.ssh("Christoph's Maven Repo", "staging", "/srv/maven/")
 }
