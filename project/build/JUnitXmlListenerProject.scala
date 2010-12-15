85caaf7f9bc881ee0dd0c72705436096895a0784
Christoph Henkelmann <christoph@henkelmann.eu>
2010-12-15 19:14:21 +0100

    changed publishing to local repo

--- project/build/JUnitXmlListenerProject.scala
+++ project/build/JUnitXmlListenerProject.scala
@@ -2,6 +2,6 @@ import sbt._
 
 
 class JUnitXmlListenerProject(info: ProjectInfo) extends PluginProject(info) {
-    override def managedStyle = ManagedStyle.Maven
-    lazy val publishTo = Resolver.ssh("Christoph's Maven Repo", "staging", "/srv/maven/")
+    override def managedStyle = ManagedStyle.Maven    
+    lazy val publishTo = Resolver.file("Development Repo", new java.io.File((Path.userHome /"srv"/"maven").toString))
 }
